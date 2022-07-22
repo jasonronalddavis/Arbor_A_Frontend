5a31641eb43ae3864a80d036e6c748663af90c73
jasonronalddavis <jdavis@c.ringling.edu>
2022-07-22 04:33:07 -0500

    first commit

---
+++ src/reducers/ROOTREDUCER.js
@@ -0,0 +1,14 @@
+import {combineReducers} from 'redux'
+
+//ACQUIRED BY REDUX STORE
+
+export const rootReducer = combineReducers({
+// users: usersReducer,
+// images: imageReducer,
+// categories: categoriesReducer,
+// signup: signupReducer,
+// storytext: storytextReducer,
+// storytexts: storytextsReducer,
+// user: userReducer,
+// LoginFormReducer
+})
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware} from "redux";
+import {rootReducer} from './reducers/ROOTREDUCER.js';
+import thunk from "redux-thunk";
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+
+
+//CONTAINS ROOT REDUCER
+
+
+const store = createStore(rootReducer,composeWithDevTools( applyMiddleware(thunk))
+
+)
+
+
+
+
+export default store
